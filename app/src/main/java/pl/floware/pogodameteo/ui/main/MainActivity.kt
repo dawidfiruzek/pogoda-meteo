e95b25a9cd565d6134ef54c6084ed3cc1227d532
Dawid Firuzek <d.firuzek@phenoma.ly>
2017-04-14 01:16:54 +0200

    Added bottom navigation to main activity

--- app/src/main/java/pl/floware/pogodameteo/ui/main/MainActivity.kt
+++ app/src/main/java/pl/floware/pogodameteo/ui/main/MainActivity.kt
@@ -1,5 +1,9 @@
 package pl.floware.pogodameteo.ui.main
 
+import android.support.design.widget.BottomNavigationView
+import android.view.MenuItem
+import butterknife.BindView
+import com.jakewharton.rxbinding2.support.design.widget.RxBottomNavigationView
 import io.reactivex.Observable
 import pl.dawidfiruzek.template.util.injection.main.DaggerMainComponent
 import pl.floware.pogodameteo.R
@@ -11,6 +15,11 @@ class MainActivity : BaseActivity(), MainContract.View, MainContract.Router {
 
     @Inject
     lateinit var presenter: MainContract.Presenter
+
+    @BindView(R.id.main_bottom_navigation)
+    lateinit var bottomNavigationView: BottomNavigationView
+
+    private var bottomNavigationObservable: Observable<MenuItem>? = null
 
     override fun getLayoutId(): Int = R.layout.activity_main
 
@@ -25,15 +34,39 @@ class MainActivity : BaseActivity(), MainContract.View, MainContract.Router {
     override fun init() {
         presenter.attachView(this)
         presenter.attachRouter(this)
+        presenter.init()
     }
 
     override fun clear() {
         presenter.clear()
     }
 
-    override fun getWeatherClickedIntent(): Observable<Boolean> = Observable.just(false)
+    override fun getWeatherClickedIntent(): Observable<Boolean> {
+        if (bottomNavigationObservable == null) {
+            initBottomNavigationObservable()
+        }
+        return bottomNavigationObservable!!.map { it.itemId == R.id.bottom_navigation_weather }
+    }
 
-    override fun getCommentClickedIntent(): Observable<Boolean> = Observable.just(false)
+    private fun initBottomNavigationObservable() {
+        bottomNavigationObservable = RxBottomNavigationView.itemSelections(bottomNavigationView)
+    }
 
-    override fun getSettingsClickedIntent(): Observable<Boolean> = Observable.just(false)
+    override fun getCommentClickedIntent(): Observable<Boolean> {
+        if (bottomNavigationObservable == null) {
+            initBottomNavigationObservable()
+        }
+        return bottomNavigationObservable!!.map { it.itemId == R.id.bottom_navigation_comment }
+    }
+
+    override fun getSettingsClickedIntent(): Observable<Boolean> {
+        if (bottomNavigationObservable == null) {
+            initBottomNavigationObservable()
+        }
+        return bottomNavigationObservable!!.map { it.itemId == R.id.bottom_navigation_settings }
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
 }
--- app/src/main/java/pl/floware/pogodameteo/ui/main/MainPresenter.kt
+++ app/src/main/java/pl/floware/pogodameteo/ui/main/MainPresenter.kt
@@ -1,10 +1,14 @@
 package pl.floware.pogodameteo.ui.main
 
 import pl.floware.pogodameteo.ui.BasePresenter
+import timber.log.Timber
 
 class MainPresenter : BasePresenter<MainContract.View, MainContract.Router>(), MainContract.Presenter {
 
     override fun init() {
+        view?.getWeatherClickedIntent()?.map { it == true }?.subscribe({ Timber.e("weather clicked") })
+        view?.getCommentClickedIntent()?.map { it == true }?.subscribe({ Timber.e("comment clicked") })
+        view?.getSettingsClickedIntent()?.map { it == true }?.subscribe({ Timber.e("settings clicked") })
         //todo init intents
     }
 }
