d7a9972d1721e1519db5ba7dc7c632d222b77cd6
Dawid Firuzek <dawid.firuzek@gmail.com>
2017-05-17 15:49:57 +0200

    Added zipped doges

--- app/src/main/java/pl/floware/pogodameteo/util/injection/AppInjection.kt
+++ app/src/main/java/pl/floware/pogodameteo/util/injection/AppInjection.kt
@@ -44,6 +44,7 @@ class AppModule(val application: BaseMeteoApp) {
     @Provides
     fun compositeDisposable(): CompositeDisposable = CompositeDisposable()
 
+    @Singleton
     @Provides
     fun imageInteractor(): ImageInteractor = ImageInteractorImpl(arrayListOf(
             "https://s-media-cache-ak0.pinimg.com/736x/3d/2b/bf/3d2bbfd73ccaf488ab88d298ab7bc2d8.jpg",
@@ -51,6 +52,7 @@ class AppModule(val application: BaseMeteoApp) {
             "http://img15.deviantart.net/97dd/i/2013/229/d/f/doge_squadron_by_shankidy-d6ijgat.png",
             "https://s-media-cache-ak0.pinimg.com/originals/ee/b7/71/eeb771122eebac79dda2eb99d43e3d82.jpg",
             "https://s-media-cache-ak0.pinimg.com/originals/24/a0/9e/24a09e49522b48d0f490de86b5d624da.jpg",
-            "http://www.imgbase.info/images/safe-wallpapers/miscellaneous/funny/48666_funny_doge.jpg",
+            "https://s-media-cache-ak0.pinimg.com/736x/55/f9/25/55f92575f958c607cf0467274d6b7539.jpg",
+            "https://s-media-cache-ak0.pinimg.com/736x/b1/f0/7a/b1f07adfb3fb75f193b0762db7e8b090.jpg",
             "https://sites.google.com/a/suffieldstudent.org/doges-com/_/rsrc/1472846231221/types-of-doges/CORN%20DOGE.jpg?height=215&width=400"))
 }
--- app/src/main/java/pl/floware/pogodameteo/util/interactor/ImageInteractorImpl.kt
+++ app/src/main/java/pl/floware/pogodameteo/util/interactor/ImageInteractorImpl.kt
@@ -1,12 +1,16 @@
 package pl.floware.pogodameteo.util.interactor
 
 import io.reactivex.Observable
+import io.reactivex.functions.BiFunction
 import java.util.concurrent.TimeUnit
 
 class ImageInteractorImpl(val images: List<String>) : ImageInteractor {
 
     override fun imageObservable(): Observable<String> {
-        return Observable.fromIterable(images)
-                .timeout(2000, TimeUnit.MILLISECONDS)
+        return Observable.zip(
+                Observable.interval(0, 3000, TimeUnit.MILLISECONDS),
+                Observable.fromIterable(images),
+                BiFunction { _, image -> image }
+        )
     }
 }
