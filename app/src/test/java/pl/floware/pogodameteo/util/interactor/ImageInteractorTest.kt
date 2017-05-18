5e5d456be8b151aa7988371b957ae8a39f565b60
Dawid Firuzek <dawid.firuzek@gmail.com>
2017-05-18 10:12:08 +0200

    Added image interactor tests

--- app/src/test/java/pl/floware/pogodameteo/util/interactor/ImageInteractorTest.kt
+++ app/src/test/java/pl/floware/pogodameteo/util/interactor/ImageInteractorTest.kt
@@ -1,4 +1,24 @@
 package pl.floware.pogodameteo.util.interactor
 
-class ImageInteractorTest {
+import org.junit.Test
+import pl.floware.pogodameteo.BaseTest
+
+class ImageInteractorTest : BaseTest() {
+
+    private lateinit var images: List<String>
+    private lateinit var interactor: ImageInteractor
+
+    override fun setup() {
+        super.setup()
+
+        images = listOf("test")
+        interactor = ImageInteractorImpl(images)
+    }
+
+    @Test
+    fun interactorTest() {
+        interactor.imageObservable()
+                .test()
+                .assertResult(images[0])
+    }
 }
